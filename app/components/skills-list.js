90da4884b9b3b0b2a8ddee92f7fab55e5aff7bd5
jchansard <josh.chansard@gmail.com>
2016-05-17 08:52:01 -0500

    display skills by type

--- app/components/skills-list.js
+++ app/components/skills-list.js
@@ -2,5 +2,29 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	tagName: 'div',
-	classNames: ['skills col-md-12']
+	classNames: ['skills col-md-12'],
+
+	sections: Ember.computed('skills', function() {
+		let sectionsObject = {};
+		let sectionsArray = [];
+		this.get('skills').forEach((skill) =>
+		{
+			let type = skill.get('type');
+			if (sectionsObject[type] === undefined) {
+				sectionsObject[type] = [skill.get('name')];
+			}
+			else
+			{
+				sectionsObject[type].push(skill.get('name'));
+			}
+		});
+		Object.keys(sectionsObject).forEach((key) =>
+		{
+			sectionsArray.push({
+				name: key,
+				skills: sectionsObject[key]
+			});
+		});
+		return sectionsArray;	
+	})
 });
--- app/models/skill.js
+++ app/models/skill.js
@@ -3,5 +3,6 @@ import attr from 'ember-data/attr';
 
 export default Model.extend({
 	name: attr('string'),
+	type: attr('string'),
 	competency: attr('string')
 });
